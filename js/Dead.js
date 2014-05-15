a6bb6834d0757b2b38c050e45a617938380ac7e1
Diego Ortín Fernández <dieortin@gmail.com>
2014-05-15 21:22:45 +0200

    Added the dead state file (neccesary in commits before this one)

---
+++ js/Dead.js
@@ -0,0 +1,23 @@
+var Dead = function(game) {
+	this.game = game;
+	this.deadText = null;
+};
+
+Dead.prototype = {
+  /*preload: function() {
+    level.preload();
+    character.preload();
+    bird.preload();
+
+  },*/
+  create:  function() {
+   //  level.create();
+   //  character.create();
+   //  bird.create();
+   this.deadText = this.game.add.text(100, this.game.world.height / 2, "You are dead", { font: "65px Arial", fill: "#ff0044", align: "center" })
+  },
+  update:  function() {
+    //bird.update();
+  	//this.deadText = this.game.add.text(100, this.game.world.height / 2, "You have died", { font: "65px Arial", fill: "#ff0044", align: "center" });
+  }
+}
