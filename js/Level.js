9e1dec6db5e4ae893cda2b076327e10b39131e2e
Diego Ortín Fernández <dieortin@gmail.com>
2014-05-19 16:59:31 +0200

    Moved spikes code to specific level code so that the position of the spikes varies depending on the level instead of being fixed

--- js/Level.js
+++ js/Level.js
@@ -3,9 +3,7 @@ Level = function(game) {
 	this.game = game;
 	this.sky = null;
 	this.house = null;
-	this.spikes = null;
 	this.floor = null;
-	this.spikesScaleFactor = 0.2;
 	this.houseScaleFactor = 0.5;
 };
 
@@ -14,7 +12,6 @@ Level.prototype = {
 	preload: function() {
 		this.game.load.image('sky', 'assets/current/background/sky.png');
 		this.game.load.image('ground', 'assets/current/background/grassV2_0.png');
-		this.game.load.image('spikes', 'assets/current/background/trapsnspikes.png');
 		this.game.load.image('house', 'assets/current/background/HouseHayRoof.png')
 	},
 
@@ -29,18 +26,6 @@ Level.prototype = {
 	    this.game.physics.enable(this.floor, Phaser.Physics.ARCADE); //Enable physics for the floor
 	    this.floor.body.setSize(this.game.world.width, 50, 0, 25); //Set collision zone
 	    this.floor.body.immovable = true; //Make the floor immovable
-	    /*--------------------------SPIKES------------------------------------*/
-	    this.spikes = this.game.add.group();
-	    this.spikes.enableBody = true;
-	    for (var i = 0; i < this.game.world.width / 600 - 1; i++) { //Number of sections minus one to have a "safe" start section
-	        var x = Math.random() * 600 + 600 * i + 600; //Generate random position inside a 600px section
-	        var spike = this.spikes.create(x, this.game.world.height - (50 + (167 * this.spikesScaleFactor) - 25), "spikes"); // 'y' position is the world height minus the floor and the spikes height (and plus the grass height)
-	        spike.body.immovable = true;
-	        spike.scale.setTo(this.spikesScaleFactor, this.spikesScaleFactor); //Scale the spikes to make them smaller
-	    }
-	},
-
-	deadCreate: function() { // To create the level when the character is dead
 	}
 
 };
--- js/Level1.js
+++ js/Level1.js
@@ -5,10 +5,16 @@ var Level1 = function(game) {
 	this.bird = null;
 	this.keys = null;
 	this.joystick = null;
+	this.spikes = null;
+	this.spikesScaleFactor = 0.2;
+	this.spikesMinDistance = 100; // Minimum distance in pixels that a spikes sprite can be from another spikes sprite
+	this.spikesPos = [3, 5, 7, 8.5, 10, 12, 16, 19.5, 22, 26]; // Sets the x position for the different spikes, used in the spikes creation loop
 }
 
 Level1.prototype = {
 	preload: function() {
+		this.game.load.image('spikes', 'assets/current/background/trapsnspikes.png');
+
 	    this.level = new Level(this.game);
 	    this.character = new Character(this.game, 100, dimensions.height - 100, 150, 500);
 	    this.bird = new Bird(this.game);
@@ -28,6 +34,15 @@ Level1.prototype = {
 	    this.level.create();
 	    this.character.create();
 	    this.bird.create();
+
+	    /* Spikes */
+	    this.spikes = this.game.add.group();
+	    this.spikes.enableBody = true;
+	    for (var i = 0; i < this.spikesPos.length; i++) { // Iterate over the position list and create spikes in each position
+	        var spike = this.spikes.create(this.spikesPos[i] * this.spikesMinDistance, this.game.world.height - (50 + (167 * this.spikesScaleFactor) - 25), "spikes"); // 'y' position is the world height minus the floor and the spikes height (and plus the grass height)
+	        spike.body.immovable = true;
+	        spike.scale.setTo(this.spikesScaleFactor, this.spikesScaleFactor); //Scale the spikes to make them smaller
+	    }
 	    this.joystick.create();
 	    //game.input.onDown.add(goFull, this);
 	},
